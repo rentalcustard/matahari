d039c836e3158930f82928da62f8edc66a306cb7
Tom Stuart <tom@therye.org>
2011-01-13 13:23:15 +0000

    Bump version

--- lib/matahari.rb
+++ lib/matahari.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Matahari
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
 
 require 'matahari/spy'
