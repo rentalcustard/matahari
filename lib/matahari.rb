5b84da158041d2b8187c592528d60604b223d69f
Tom Stuart <tom@therye.org>
2011-01-13 12:58:14 +0000

    More formatting

--- lib/matahari.rb
+++ lib/matahari.rb
@@ -2,7 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module Matahari
-	VERSION = '0.0.1'
+  VERSION = '0.0.1'
 end
 
 require 'matahari/spy'
@@ -10,5 +10,5 @@ require 'matahari/debriefing'
 require 'matahari/rspec/matchers'
 
 def spy(name = nil)
-	Spy.new(name)
+  Spy.new(name)
 end
--- lib/matahari/rspec/matchers.rb
+++ lib/matahari/rspec/matchers.rb
@@ -1,10 +1,10 @@
 def have_received(times = nil)
-	if times 
-		@calls_expected = 0
-		times.each { @calls_expected+= 1 }
+  if times 
+    @calls_expected = 0
+    times.each { @calls_expected+= 1 }
 
-		Debriefing.new(@calls_expected)
-	else
-		Debriefing.new
-	end
+    Debriefing.new(@calls_expected)
+  else
+    Debriefing.new
+  end
 end
