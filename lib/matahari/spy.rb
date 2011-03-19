90e6f16d7aa9b2b5e2127bc3576b44d2269c370b
Tom Stuart <tom@therye.org>
2011-03-19 11:08:52 +0000

    Change instance var -> local

--- lib/matahari/spy.rb
+++ lib/matahari/spy.rb
@@ -22,10 +22,10 @@ class Spy
 
   def has_received?(times=nil)
     if times 
-      @calls_expected = 0
-      times.each { @calls_expected+= 1 }
+      calls_expected = 0
+      times.each { calls_expected += 1 }
 
-      Debriefing.new(@calls_expected)
+      Debriefing.new(calls_expected)
     else
       Debriefing.new
     end
