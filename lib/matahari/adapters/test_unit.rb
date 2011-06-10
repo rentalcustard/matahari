3856194a9b691fec0d394bfdcb362dc8a37e509b
Tom Stuart <tom@therye.org>
2011-06-10 14:38:05 +0100

    assert_not_received method

--- lib/matahari/adapters/test_unit.rb
+++ lib/matahari/adapters/test_unit.rb
@@ -5,8 +5,13 @@ module Matahari
 
       def assert_received(subject, &block)
         result = block.call
-        assert result.matches?(subject), "#{result.failure_message_for_should}"
+        assert result.matches?(subject), result.failure_message_for_should
       end
+
+			def assert_not_received(subject, &block)
+				result = block.call
+				assert !result.matches?(subject), result.failure_message_for_should_not
+			end
     end
   end
 end
--- spec/matahari/adapters/test_unit_spec.rb
+++ spec/matahari/adapters/test_unit_spec.rb
@@ -2,32 +2,54 @@ require 'spec_helper'
 require 'test/unit'
 
 class DummyTestCase < Test::Unit::TestCase
-	include Matahari::Adapters::TestUnit
+  include Matahari::Adapters::TestUnit
 end
 
 describe Matahari::Adapters::TestUnit do
-	before(:each) do
-		@dummy = DummyTestCase.new "dummy"
-	end
+  before(:each) do
+    @dummy = DummyTestCase.new "dummy"
+  end
 
-	describe "#assert_received" do
-		context "with a passing test" do
-			it "passes" do
-				subject = spy(:matahari)
+  describe "#assert_received" do
+    context "with a passing test" do
+      it "passes" do
+        subject = spy(:matahari)
 
-				subject.some_method
-			
-			  lambda { @dummy.assert_received(subject) { subject.has_received?.some_method } }.should_not raise_error
-			end
+        subject.some_method
 
-			context "with a failing test" do
-				it "gives an informative message when it fails" do
-					subject = spy(:matahari)
+        lambda { @dummy.assert_received(subject) { subject.has_received?.some_method } }.should_not raise_error
+      end
 
-					lambda { @dummy.assert_received(subject) { subject.has_received?.some_method } }.
-						should raise_error(MiniTest::Assertion, "Spy(:matahari) expected to receive :some_method once, received 0 times")
-				end
-			end
-		end
-	end
+      context "with a failing test" do
+        it "fails with an informative message" do
+          subject = spy(:matahari)
+
+          lambda { @dummy.assert_received(subject) { subject.has_received?.some_method } }.
+            should raise_error(MiniTest::Assertion, "Spy(:matahari) expected to receive :some_method once, received 0 times")
+        end
+      end
+    end
+  end
+
+  describe "#assert_not_received" do
+    context "with a passing test" do
+      it "passes" do
+        subject = spy(:matahari)
+
+        lambda { @dummy.assert_not_received(subject) { subject.has_received?.some_method } }.
+          should_not raise_error
+      end
+    end
+
+    context "with a failing test" do
+      it "fails with an informative message" do
+        subject = spy(:matahari)
+
+        subject.some_method
+
+        lambda { @dummy.assert_not_received(subject) { subject.has_received?.some_method } }.
+          should raise_error(MiniTest::Assertion, "Spy(:matahari) expected not to receive :some_method but received it once")
+      end
+    end
+  end
 end
