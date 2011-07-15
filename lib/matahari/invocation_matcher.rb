8d00838d899e9d3c7a3c227094dd1b1ec9a35751
Tom Stuart <tom@therye.org>
2011-07-15 10:13:49 +0100

    Explanatory comment

--- lib/matahari/invocation_matcher.rb
+++ lib/matahari/invocation_matcher.rb
@@ -30,6 +30,8 @@ module Matahari
     #e.g. spy.should_have received.some_method, where #some_method is handled by
     #method_missing, its arguments and name being stored until #matches? is called.
     def method_missing(sym, *args, &block)
+      #when args are supplied, we need to wrap them in an array because Invocation's #initialize
+      #flattens them by one step
       @expected_invocation = args.size > 0 ? Matahari::Invocation.new(sym, [args]) : Matahari::Invocation.new(sym)
       self
     end
