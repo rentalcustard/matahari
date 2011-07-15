0249cbe585e6680d3cea48cc47b83834feed51f1
Tom Stuart <tom@therye.org>
2011-07-15 10:11:03 +0100

    Refactoring: remove redundant ivars

--- lib/matahari/invocation_matcher.rb
+++ lib/matahari/invocation_matcher.rb
@@ -6,25 +6,23 @@ module Matahari
 
     def matches?(subject)
       @subject = subject
-      invocations = subject.invocations
-      verifying_args = @args_to_verify.size != 0
 
-      @matching_calls = verifying_args ? 
-        invocations.select {|i| @expected_invocation == i } :
-        invocations.select {|i| @expected_invocation === i }
+      @matching_calls = @expected_invocation.args ? 
+        @subject.invocations.select {|i| @expected_invocation == i } :
+        @subject.invocations.select {|i| @expected_invocation === i }
 
       match_passes?(@matching_calls)
     end
 
     def failure_message_for_should_not
-      "Spy(:#{@subject.name}) expected not to receive :#{@call_to_verify} but received it #{prettify_times(@matching_calls.size)}"
+      "Spy(:#{@subject.name}) expected not to receive :#{@expected_invocation.method} but received it #{prettify_times(@matching_calls.size)}"
     end
 
     def failure_message_for_should
-      if @args_to_verify.size > 0
-        "Spy(:#{@subject.name}) expected to receive :#{@call_to_verify}(#{@args_to_verify.map(&:inspect).join(", ")}) #{prettify_times(@expected_call_count)}, received #{prettify_times(@matching_calls.size)}"
+      if @expected_invocation.args
+        "Spy(:#{@subject.name}) expected to receive :#{@expected_invocation.method}(#{@expected_invocation.args.map(&:inspect).join(", ")}) #{prettify_times(@expected_call_count)}, received #{prettify_times(@matching_calls.size)}"
       else
-        "Spy(:#{@subject.name}) expected to receive :#{@call_to_verify} #{prettify_times(@expected_call_count)}, received #{prettify_times(@matching_calls.size)}"
+        "Spy(:#{@subject.name}) expected to receive :#{@expected_invocation.method} #{prettify_times(@expected_call_count)}, received #{prettify_times(@matching_calls.size)}"
       end
     end
 
@@ -32,10 +30,7 @@ module Matahari
     #e.g. spy.should_have received.some_method, where #some_method is handled by
     #method_missing, its arguments and name being stored until #matches? is called.
     def method_missing(sym, *args, &block)
-      @call_to_verify = sym
-      @args_to_verify = args
-
-      @expected_invocation = args ? Matahari::Invocation.new(sym, [args]) : Matahari::Invocation.new(sym)
+      @expected_invocation = args.size > 0 ? Matahari::Invocation.new(sym, [args]) : Matahari::Invocation.new(sym)
       self
     end
 
