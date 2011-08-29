2d08f8ec9bba3a8352dcb389504fe14aab5fff23
deepj <deepjungle.maca@gmail.com>
2011-08-29 14:56:48 +0200

    Simplify some code in spy class

--- lib/matahari/spy.rb
+++ lib/matahari/spy.rb
@@ -1,22 +1,24 @@
 module Matahari
   class Spy
+    METHOD_WHITELIST = [:name, :define_method, :stubs, :passes_on, :method_missing, :record_invocation, :invocations,
+                        :has_received?, :object_id, :respond_to?, :respond_to_missing?, :instance_eval, :instance_exec,
+                        :class_eval, :__send__, :send, :should, :should_not, :__id__, :__send__] 
+
     attr_reader :name, :invocations
 
-    def initialize(name = nil, opts={})
+    def initialize(name = nil, opts = {})
       @subject = opts[:subject]
-      @name = name
-      @invocations = []
+      @name    = name
+
+      @invocations   = []
       @stubbed_calls = {}
+
       class << self
-        instance_methods.each do |meth|
-          whitelist = [:name, :define_method, :stubs, :passes_on, :method_missing, :record_invocation, :invocations, :has_received?, :object_id, :respond_to?, :respond_to_missing?, :instance_eval, :instance_exec, :class_eval, :__send__, :send, :should, :should_not, :__id__, :__send__]
-          next if whitelist.include?(meth) || whitelist.include?(meth.to_sym)
-          undef_method(meth)
-        end
+        (instance_methods - METHOD_WHITELIST).each { |method| undef_method(method) }
       end
     end
 
-    #When a given method call, sym, is invoked on self, call block and return its result
+    # When a given method call, sym, is invoked on self, call block and return its result
     def stubs(sym, &block)
       @stubbed_calls[sym] = block
     end
@@ -25,17 +27,17 @@ module Matahari
       @stubbed_calls[sym] = @subject.method(sym)
     end
 
-    #Captures the details of any method call and store for later inspection
+    # Captures the details of any method call and store for later inspection
     def method_missing(sym, *args, &block)
       record_invocation(sym, args)
       @stubbed_calls[sym].call if @stubbed_calls[sym]
     end
 
-    #Pass an iterator to this method to specify the number of times the method should
-    #have been called. E.g. spy.has_received?(3.times). While other iterators might work,
-    #the idea is to allow this nice DSL-ish way of asserting on the number of calls, hence
-    #the odd method signature.
-    def has_received?(times=nil)
+    # Pass an iterator to this method to specify the number of times the method should
+    # have been called. E.g. spy.has_received?(3.times). While other iterators might work,
+    # the idea is to allow this nice DSL-ish way of asserting on the number of calls, hence
+    # the odd method signature.
+    def has_received?(times = nil)
       InvocationMatcher.new(times)
     end
 
